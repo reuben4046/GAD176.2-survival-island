130d00199754683efb335fb1ed7f7f4218fb0d25
SAE-1032443 <1032443@student.sae.edu.au>
2024-07-17 16:29:51 +1000

    Added Timer

---
+++ Assets/Mitchell/Scripts/Timer.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    float secondtimer;
+    int second;
+    int minute;
+    [SerializeField] float clockspeed = 1;
+    private Text clocktext;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clocktext = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        secondtimer += Time.deltaTime * clockspeed;
+
+        if (secondtimer >= 1f)
+        {
+            second++;
+            if (second == 60)
+            {
+                minute++;
+                second = 0;
+            }
+
+            if (minute == 24)
+            {
+                minute = 0;
+            }
+
+            secondtimer = 0;
+
+            clocktext.text = $"{minute}:{second}";
+        }
+    }
+}
