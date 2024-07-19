ef9b18497cc9c5db1f42101d76ea8f898806e101
reuben4046 <poolereuben@gmail.com>
2024-07-19 17:15:21 +1000

    saved

--- Assets/Reuben/Scripts/MapGenerator.cs
+++ Assets/Reuben/Scripts/MapGenerator.cs
@@ -8,6 +8,7 @@ public class MapGenerator : MonoBehaviour
     public int mapHeight;
     public float noiseScale;
     public int octaves;
+    [Range(0, 1)]
     public float persistance;
     public float lacunarity;
 
@@ -22,5 +23,26 @@ public class MapGenerator : MonoBehaviour
 
         MapDisplay display = FindObjectOfType<MapDisplay>();
         display.DrawNoiseMap(noiseMap);
+    }
+
+    void OnValidate()
+    {
+        if (mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+        if (mapHeight < 1)
+        {
+            mapHeight = 1;
+        }
+        if (lacunarity < 1)
+        {
+            lacunarity = 1;
+        }
+        if (octaves < 0)
+        {
+            octaves = 0;
+        }
+        
     }
 }
