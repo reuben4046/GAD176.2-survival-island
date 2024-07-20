8b838ed08ebab9b1a0812d4d5307d569f7067c22
reuben4046 <poolereuben@gmail.com>
2024-07-20 12:36:18 +1000

    mesh generating in multiple chunks

--- Assets/Reuben/Scripts/EndlessTerrain.cs
+++ Assets/Reuben/Scripts/EndlessTerrain.cs
@@ -7,6 +7,7 @@ public class EndlessTerrain : MonoBehaviour
 {
     public const float maxViewDistance = 450;
     public Transform veiwer;
+    public Material mapMaterial;
 
     public static Vector2 viewerPosition;
     static MapGenerator mapGenerator;
@@ -54,7 +55,7 @@ public class EndlessTerrain : MonoBehaviour
                     }
                 } else
                 {
-                    terrainChunkDictionary.Add(veiwedChunkCoord, new TerrainChunk(veiwedChunkCoord, chunkSize, transform));
+                    terrainChunkDictionary.Add(veiwedChunkCoord, new TerrainChunk(veiwedChunkCoord, chunkSize, transform, mapMaterial));
                 }
             } 
         }
@@ -66,15 +67,23 @@ public class EndlessTerrain : MonoBehaviour
         Vector2 position;
         Bounds bounds;
 
-        public TerrainChunk(Vector2 coord, int size, Transform parent)
+        MapData mapData;
+
+        MeshRenderer meshRenderer;
+        MeshFilter meshFilter;
+
+        public TerrainChunk(Vector2 coord, int size, Transform parent, Material material)
         {
             position = coord * size;
             bounds = new Bounds(position, Vector2.one * size);
             Vector3 positionV3 = new Vector3(position.x, 0, position.y);
 
-            meshObject = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            meshObject = new GameObject("Terrain Chunk");
+            meshRenderer = meshObject.AddComponent<MeshRenderer>();
+            meshFilter = meshObject.AddComponent<MeshFilter>();
+            meshRenderer.material = material;
+
             meshObject.transform.position = positionV3;
-            meshObject.transform.localScale = Vector3.one * size / 10f;
             meshObject.transform.parent = parent;
             SetVisable(false);
 
@@ -83,7 +92,12 @@ public class EndlessTerrain : MonoBehaviour
 
         void OnMapDataReceived(MapData mapData)
         {
-            print("map data received");
+            mapGenerator.RequestMeshData(mapData, OnMeshDataReceived);
+        }
+
+        void OnMeshDataReceived(MeshData meshData)
+        {
+            meshFilter.mesh = meshData.CreateMesh();
         }
 
         public void UpdateTerrainChunk()
