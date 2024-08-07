b6fff28a435805f17f4b21e3fac6c9cdc7fae747
SAE-1032443 <1032443@student.sae.edu.au>
2024-08-07 19:54:52 +1000

    Began Working on the Rain

--- Assets/Mitchell/Scripts/WeatherPattern.cs
+++ Assets/Mitchell/Scripts/WeatherPattern.cs
@@ -6,6 +6,7 @@ public class WeatherPattern : MonoBehaviour
 {
     public string weatherName;
     public Texture2D skyBox;
+    public GameObject weather;
 
     // Start is called before the first frame update
     void Start()
